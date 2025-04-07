1ffea64a5117631b3e56dedf417eda3c1f234e72
Yi Hang <yihang19990726@gmail.com>
2025-04-07 10:06:44 +0800

    api, android emulator fix

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_time_app/services/world_time.dart';
+// ignore_for_file: avoid_print
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -15,9 +16,16 @@ class _LoadingState extends State<Loading> {
   void setupWorldTime() async {
     WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
     await instance.getTime();
-    print(instance.time);
-    setState(() {
-      time = instance.time;
+    //print(instance.time);
+    // setState(() {
+    //   time = instance.time;
+    // });
+
+    // ignore: use_build_context_synchronously
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time
     });
   }
 
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart';
-
+// ignore_for_file: avoid_print
 class WorldTime {
   late String location;
   late String time;
@@ -13,19 +13,24 @@ class WorldTime {
     try {
       //make the request
       Response response =
-          await get(Uri.parse("http://worldtimeapi.org/api/timezone/$url"));
+          await get(Uri.parse("https://www.timeapi.io/api/time/current/zone?timeZone=$url"));
 
+      //await get(Uri.parse("http://worldtimeapi.org/api/timezone/$url"));
       Map data = jsonDecode(response.body);
 
-      String datetime = data['datetime'];
-      String offset = data['utc_offset'].substring(1, 3);
+      print(data);
 
-      DateTime now = DateTime.parse(datetime);
 
-      now = now.add(Duration(hours: int.parse(offset)));
+
+      // Parse the datetime (assumes it's in UTC if no timezone is specified)
+      DateTime parsedTime = DateTime.parse(data['dateTime']!);
+
+      // Apply the UTC offset (e.g., +2 hours for CEST)
+      const utcOffset = Duration(hours: 6); // From dstActive: true
+      final adjustedTime = parsedTime.add(utcOffset);
 
       //set the time properly
-      time = now.toString();
+      time = adjustedTime.toString();
     } catch (e) {
       print('caught error: $e');
       time = 'Could not get time data';
